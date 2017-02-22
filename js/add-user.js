678d215646da3121a927fcb1acf3c11ccf379fe0
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2017-02-22 06:42:33 +0200

    added the method isPhone, changed methods createMain and render

--- js/add-user.js
+++ js/add-user.js
@@ -1,22 +1,39 @@
 class AddUser {
 	constructor() {
 		this.newUser = {};
+	
 	}
 	createHeader() {
 		return `<header class="header">
 		<div class="container top-radius">
 			<nav class="user-top-line">
-				<a href="user.html" id = "cansel">Cansel</a>
+				<a href="user.html" id = "cancel">Cancel</a>
 				<button class="done-btn" id = "done">Done</button>
 			</nav>
 		</div>
 	</header>`;
 	}
 
-
+	isPhone() {
+		let string = '';
+		if (this.phone) {
+			string += `<div class="edit-field">
+						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+						<label class="sr-only" for="phone">Add phone</label>
+						<input type="text" class="add-btn" id="phone" value = "${this.phone}">
+					</div>`;
+		} else {
+			string += `<div class="edit-field">
+						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+						<label class="sr-only" for="phone">Add phone</label>
+						<input type="text" class="add-btn" id="phone" placeholder="add phone">
+					</div>`;
+		}
+		return string
+	}
 	createMain() {
-
-		return `<main class="main add-user" id= "main">
+		let main = '';
+		main += `<main class="main add-user" id= "main">
 		<div class="container">
 			<div class="edit-main-info">
 				<div class="edit-foto">
@@ -42,44 +59,41 @@ class AddUser {
 				</div>
 			</div>
 			<div class="scroll-holder">
-				<div class="edit-info">
-					<div class="edit-field">
-						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-						<label class="sr-only" for="phone">Add phone</label>
-						<input type="text" class="add-btn" id="phone" placeholder="add phone">
+				<div class="edit-info">`;
+		main += this.isPhone();
+		main += `<div class="edit-field">
+			<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+			<label class="sr-only" for="email">Add email</label>
+			<input type="text" class="add-btn" id="email" placeholder="add email">
 					</div>
-					<div class="edit-field">
-						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-						<label class="sr-only" for="email">Add email</label>
-						<input type="text" class="add-btn" id="email" placeholder="add email">
+			<div class="edit-field">
+				<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+				<label class="sr-only" for="address">Add address</label>
+				<input type="text" class="add-btn" id="address" placeholder="add address">
 					</div>
-					<div class="edit-field">
-						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-						<label class="sr-only" for="address">Add address</label>
-						<input type="text" class="add-btn" id="address" placeholder="add address">
-					</div>
-					<div class="edit-field">
-						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-						<label class="sr-only" for="birthday">Add birthday</label>
-						<input type="text" class="add-btn" id="birthday" placeholder="add birthday">
+				<div class="edit-field">
+					<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+					<label class="sr-only" for="birthday">Add birthday</label>
+					<input type="text" class="add-btn" id="birthday" placeholder="add birthday">
 					</div>
 					<div class="edit-field">
 						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
 						<label class="sr-only" for="socialProfile">Add social profile</label>
 						<input type="text" class="add-btn" id="socialProfile" placeholder="add social profile">
 					</div>
-					<div class="edit-field">
-						<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-						<label class="sr-only" for="addField">Add field</label>
-						<input type="text" class="add-btn" id="addField" placeholder="add field">
+						<div class="edit-field">
+							<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+							<label class="sr-only" for="addField">Add field</label>
+							<input type="text" class="add-btn" id="addField" placeholder="add field">
 					</div>
-					<div class="edit-field">
-						<button href="#" class="delete-contact">delete contact</button>
+							<div class="edit-field">
+								<button href="#" class="delete-contact">delete contact</button>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</main>`
+	</main>`;
+		return main
 	}
 
 	events() {
@@ -87,6 +101,13 @@ class AddUser {
 		this.done = document.getElementById('done');
 		this.main = document.getElementById('main');
 		this.inputs = [...this.main.getElementsByTagName('input')];
+
+		this.cancel.addEventListener('click', e => {
+			e.preventDefault();
+			let myUser = new User(this.user);
+			myTelephoneBook.render();
+		});
+
 		this.done.addEventListener('click', e => {
 			const url = 'https://easycode-js.herokuapp.com/alexm/users';
 			this.inputs.forEach(elem => {
@@ -110,13 +131,14 @@ class AddUser {
 					myTelephoneBook.request();
 				})
 		});
-		}
+	}
 
-	render() {
-				let app = document.getElementById('app')
+	render(phoneNumber) {
+		this.phone = phoneNumber;
+		let app = document.getElementById('app')
 		app.innerHTML = this.createHeader() + this.createMain();
-				this.events();
-			}
+		this.events();
+	}
 }
 
 let myAddUser = new AddUser();
